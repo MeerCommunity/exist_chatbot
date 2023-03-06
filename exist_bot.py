fdd54782fad8b4ac101e19ec9f943a9430c869a8
MeerCommunity <84769922+MeerCommunity@users.noreply.github.com>
2023-03-06 16:02:56 +0100

    Update exist_bot.py

--- exist_bot.py
+++ exist_bot.py
@@ -247,7 +247,7 @@ if __name__== '__main__':
     st.markdown(f"<div style='{header_bg}'>", unsafe_allow_html=True)
     col1, col2, col3 = st.columns([1, 2,1])
     with col1:
-        st.markdown(f'<a href="https://www.meercommunity.de/booking/?_gl=1*3tmwvy*_ga*MjU0NTUwNjgxLjE2NzgxMTQ3NzU.*_up*MQ.."><img src="https://www.meercommunity.de/wp-content/uploads/2022/07/Termin_vereinbaren_rund.png" style="width: 100px;></a>', unsafe_allow_html=True)
+        st.markdown(f'<a href="https://www.meercommunity.de/booking/?_gl=1*3tmwvy*_ga*MjU0NTUwNjgxLjE2NzgxMTQ3NzU.*_up*MQ.."><img src="https://www.meercommunity.de/wp-content/uploads/2022/07/Termin_vereinbaren_rund.png" style="width: 100px></a>', unsafe_allow_html=True)
     with col2:
         
         st.markdown(f'<p style="color:{text_color};font-size:72px;border-radius:2%;text-align:center;">EXIST-Chatbot</p>', unsafe_allow_html=True)
