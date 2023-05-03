78ed98e57f51b97f127204f4d1b3518d52dc3974
smallwest888 <63802737+smallwest888@users.noreply.github.com>
2023-05-03 12:16:26 +0200

    IPRO-Demo Usable

--- exist_bot.py
+++ exist_bot.py
@@ -7,25 +7,21 @@ import openai
 
 def generate_response(user_input):
     # 用您的OpenAI API密钥进行身份验证
-    openai.api_key = "your_openai_api_key_here"
+    openai.api_key = "sk-"
 
     # 设置GPT-3模型的相关参数
     model_engine = "gpt-3.5-turbo"
-    prompt = "Antwort auf Deutsch, Sie vertreten die Hochschule Emden/Leer, Halten Sie Ihre Antworten so kurz wie möglich, Ihr Name ist IPRO-ChatBot"
-    max_tokens = 100
 
-    # 调用OpenAI API生成回答
-    response = openai.Completion.create(
-        engine=model_engine,
-        prompt=prompt,
-        max_tokens=max_tokens,
-        n=1,
-        stop=None,
-        temperature=0.8,
+    response = openai.ChatCompletion.create(
+        model=model_engine,
+        messages=[
+            {"role": "system", "content": "Antwort auf Deutsch, Sie vertreten die Hochschule Emden/Leer, Halten Sie Ihre Antworten so kurz wie möglich, Ihr Name ist IPRO-ChatBot"},
+            {"role": "user", "content": user_input},
+        ],
     )
 
     # 提取并返回生成的文本
-    return response.choices[0].text.strip()
+    return response.choices[0].message.content.strip()
 
 
 
