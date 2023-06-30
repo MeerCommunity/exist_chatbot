a10b65c8b3d2c6c154494635a729c91f8c74ab85
smallwest888 <63802737+smallwest888@users.noreply.github.com>
2023-06-30 11:46:56 +0200

    File Reader_Pre2.2

--- exist_bot.py
+++ exist_bot.py
@@ -32,6 +32,7 @@ def generate_response(user_input):
         If you don't know the answer, just say you don't know. Do NOT try to make up an answer.
         If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
         Use as much detail as possible when responding.
+        Alle Antworten können sich nur auf vorhandene Dokumente stützen
 
 
         context: {context}
