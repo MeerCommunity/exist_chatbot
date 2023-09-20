d51f94601e499a1ee9bc15c477a683a22cca30d7
smallwest888 <63802737+smallwest888@users.noreply.github.com>
2023-09-21 00:49:23 +0200

    File Reader_GPT 3.5_V1.5

--- exist_bot.py
+++ exist_bot.py
@@ -10,6 +10,9 @@ import numpy as np
 from openai.embeddings_utils import cosine_similarity
 import PyPDF2
 
+# print("Current Working Directory:", os.getcwd())
+# print("Files in Directory:", os.listdir())
+
 def get_pdf_content(file_path):
     pdf_file_obj = open(file_path, 'rb')
     pdf_reader = PyPDF2.PdfReader(pdf_file_obj)
@@ -44,8 +47,10 @@ def generate_response(user_input):
         ======
         """
 
+    pdf_file_name = predict_intent_with_gpt(user_input)
+    # print("Selected PDF File Name:", pdf_file_name)
     # load PDF content
-    pdf_content = get_pdf_content("Liste_Fragen_IuP KSpethmann.pdf")
+    pdf_content = get_pdf_content(pdf_file_name)
 
     response = openai.ChatCompletion.create(
         model=model_engine,
@@ -59,6 +64,62 @@ def generate_response(user_input):
     return response.choices[0].message.content.strip()
 
 st.title("IPRO-Demo")
+st.info("Bitte beachten Sie: Die Antworten, die von diesem Chatbot gegeben werden, basieren auf AI und sind möglicherweise nicht immer 100% genau oder zuverlässig. Bei Unklarheiten oder wichtigen Anfragen empfehlen wir, sich direkt an die zuständige Stelle zu wenden.")
+
+def get_pdf_content(file_path):
+    file_path = file_path.strip("'")  # 添加这一行来去除任何额外的单引号
+    pdf_file_obj = open(file_path, 'rb')
+    pdf_reader = PyPDF2.PdfReader(pdf_file_obj)
+    num_pages = len(pdf_reader.pages)
+    text_content = ""
+    for page in range(num_pages):
+        page_obj = pdf_reader.pages[page]
+        text_content += page_obj.extract_text()
+    pdf_file_obj.close()
+    return text_content
+
+def predict_intent_with_gpt(question):
+    """
+    Predicting user question intent using ChatGPT
+    """
+    valid_intents = ["Contact", "Transport", "Main"]
+    max_attempts = 2
+    attempts = 0
+
+    while attempts < max_attempts:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system",
+                 "content": "Predict the intent of the question. The answer must be one of the following: 'Contact.pdf', 'Anreise.pdf', or 'Main.pdf'."},
+                {"role": "user", "content": question},
+            ]
+        )
+
+        # Access to forecast results
+        predicted_intent = response.choices[0].message.content.strip()
+
+        if predicted_intent in valid_intents:
+            return predicted_intent
+
+        attempts += 1
+
+    return "Main.pdf"
+
+# def select_pdf_based_on_question(question):
+#     """
+#     Wählen Sie das entsprechende PDF-Dokument entsprechend dem Inhalt der Frage aus.
+#     """
+#     predicted_intent = predict_intent_with_gpt(question)
+#
+#     # Map the predicted intent to the correct PDF file name
+#     pdf_mapping = {
+#         "Contact": "Contact.pdf",
+#         "Transport": "Anreise.pdf",
+#         "Main": "Main.pdf"
+#     }
+#
+#     return pdf_mapping.get(predicted_intent, "Main.pdf")  # 默认返回"Main.pdf"，以防万一
 
 # Initialize chat history in session state
 if 'chat_history' not in st.session_state:
